ecccd309785f4beace0abc50c18a20e4c071b917
Jesse <jsebaltazar@gmail.com>
2019-12-31 22:04:08 -0700

    Integrated mailchimp api

--- app.js
+++ app.js
@@ -7,22 +7,69 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
 
+
+
 app.post("/", function(req, res) {
-  console.log(req.body.Fname);
-  console.log(req.body.Lname);
-  console.log(req.body.email);
-  res.send("Thanks!");
+  var firstName = req.body.Fname;
+  var lastName = req.body.Lname;
+  var email = req.body.email;
+
+  var data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }]
+  }
+
+  var options = {
+    url: 'https://us4.api.mailchimp.com/3.0/lists/d733e88c13',
+    method: 'POST',
+    headers: {
+      "Authorization": "jesse1 126819ee99dbb50a46a126229e6f0d6c-us4"
+    },
+    //You need to stringify JavaScript Object
+    body: JSON.stringify(data)
+  };
+
+  request(options, function(error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(response.statusCode);
+    }
+
+  });
+
+
+
+  console.log(firstName);
+  console.log(lastName);
+  console.log(email);
+
+  res.sendFile(__dirname + "/success.html");
 
 });
+
 
 
 app.listen(port, function(req, res) {
   console.log(`Server started @ port ${port}`);
 });
+
+//ID
+//126819ee99dbb50a46a126229e6f0d6c-us4
+//API
+//7130361748
